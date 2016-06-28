131a9b3a86de7bcaf22df0efcbe441229306e7eb
Esperanza <hola@mi-mina.com>
2016-06-28 11:47:37 +0200

    create a repo for learning componentes from Sierra

---
+++ test/hola_components/core_test.clj
@@ -0,0 +1,7 @@
+(ns hola-components.core-test
+  (:require [clojure.test :refer :all]
+            [hola-components.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
